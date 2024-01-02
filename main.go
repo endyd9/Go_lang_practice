177e6f1ed5dbf95f4ff4418c1850e61f9616634d
endyd9 <endyd9@gmail.com>
2024-01-02 13:16:33 +0900

    url checker without go routines

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	mydict "github.com/endyd9/learngo/accounts/dict"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "first")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(word)
+var errRequestFaild = errors.New("Request Faild")
 
+func main() {
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILD"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking", url)
+	resp, err:= http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFaild
+	}
+	return nil
 }
